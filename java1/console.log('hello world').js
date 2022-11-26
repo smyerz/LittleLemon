7baadd2484827176ee513b8c82b7a20cd82b35c7
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-26 14:07:57 -0800

    commit4

--- java1/console.log('hello world').js
+++ java1/console.log('hello world').js
@@ -8,3 +8,43 @@ comment
 */
 /* this is a multi-line comment on a single line */
 console.log("Hello " + "there " + "World");
+//string
+"Hello World"
+//number 472
+/* arithmentic operators
+ =, +, /, *, 
+comparison operators
+<, >, !=, ==, 
+logical operators
+and &&, example a>5 && a<10
+! not
+|| or
+*/
+/* Pet is hungry: true
+Feeding the pet
+Pet is hungry: false
+true
+*/
+/*
+Logical AND operator: && 
+
+Logical OR operator: || 
+
+Logical NOT operator: ! 
+
+The modulus operator: % 
+
+The equality operator: ==
+
+The strict equality operator: ===
+
+The inequality operator: !=
+
+The strict inequality operator: !==
+
+The addition assignment operator: += 
+
+The concatenation assignment operator: +=
+*/
+"use strict";
+console.log(7 == 7);
