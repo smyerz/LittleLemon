fcc751e88ecb311b11f641f5ec9c95d5000cbdc3
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-26 16:43:28 -0800

    commit8

--- java1/console.log('hello world').js
+++ java1/console.log('hello world').js
@@ -7,7 +7,7 @@ multi-line
 comment
 */
 /* this is a multi-line comment on a single line */
-//example of a console.log:
+//console.log example:
 console.log("Hello " + "there " + "World");
 //string "Hello"
 //number 472
@@ -26,25 +26,45 @@ Pet is hungry: false
 true
 */
 /*
-Logical AND operator: && 
-
+Logical AND operator (ampersand &): && 
 Logical OR operator: || 
-
 Logical NOT operator: ! 
-
 The modulus operator: % 
-
+    (example: answer depends on how many times does 6 go into 12.
+    12%6 
+    answer: 0 because 6 goes into 12 2 times with no remanders
+    14%6
+    answer: 2 because 2 is the remainder)
 The equality operator: ==
-
 The strict equality operator: ===
-
+        equal to value first and type second
 The inequality operator: !=
-
 The strict inequality operator: !==
-
+        does not equal value or type
 The addition assignment operator: += 
-
 The concatenation assignment operator: +=
 */
-//"use strict";
-//console.log(7 == 7);
+/* console.log example:
+"use strict";
+console.log(7 == 7);
+*/
+/* setting variables:
+var example = value
+*/
+/*
+Create a variable named  score and set it to  8
+Use console.log() that includes the string "Mid-level skills:" and compares the score variable to above 0 and below 10 using the && operator
+The expected output in the console should be: "Mid-level skills: true".
+*/
+//answer
+/*
+var score = 8
+console.log("mid-level skills:", score>0 && score<10);
+*/
+//answer: mid-level skills: true
+/*
+var timeRemaining = 0
+var energy = 10
+console.log("game over", + timeRemaining == 0 || energy == 0)
+*/
+//answer: game over true
