fcd6320590b25dc1970573b30717c8c08b67cff7
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-27 14:39:42 -0800

    commit2

---
+++ ifstatements/elseif.js
@@ -0,0 +1,11 @@
+var place = 'first';
+
+if(place == 'first') {
+    console.log('gold!')
+}else if (place == 'second') {
+    console.log('silver')
+}else if (place == 'third') {
+    console.log('bronze') 
+} else {
+    console.log('sorry')
+}
