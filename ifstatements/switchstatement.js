3ab2da4c90e38ec1ea5fac1c7956e12f90ccf3c6
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-27 14:39:30 -0800

    commit1

---
+++ ifstatements/switchstatement.js
@@ -0,0 +1,16 @@
+//switch statement
+var place = 'first';
+
+switch(place) {
+    case 'first':
+        console.log('gold!');
+        break;
+    case 'second':
+        console.log('silver');
+        break;
+    case 'third':
+        console.log('bronze');
+        break;
+    default:
+        console.log("sorry, you'll get it next time!")
+}
