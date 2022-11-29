d15454988f2544378dbddff8cf3244f441a927c3
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-29 12:25:01 -0800

    commit3

---
+++ ifstatements/ifelse.js
@@ -0,0 +1,24 @@
+//if else (if and then else if statements)
+var place = 'first';
+
+if(place == 'first') {
+    console.log('gold!')
+}else if (place == 'second') {
+    console.log('silver')
+}else if (place == 'third') {
+    console.log('bronze') 
+} else {
+    console.log('sorry')
+}
+//example2
+var age = 65
+
+if(age >= 65) {
+    console.log("You get income from your pension")
+}else if(age <65, age >18) {
+    console.log("Each month you get a salary")
+}else if(age<18) {
+    console.log("You get an allowance")
+}else {
+    console.log("The value of the age is not numerical")
+}
