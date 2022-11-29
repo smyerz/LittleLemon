5bd2692bcdd563e2d0e311568553ec65c26d6db6
maugrins <37156396+smyerz@users.noreply.github.com>
2022-11-29 12:24:31 -0800

    Commit2

---
+++ ifstatements/nestedLoops.js
@@ -0,0 +1,36 @@
+//examples of nested loop (resource intensive)
+for (var i =2023; i <2025; i ++){
+    console.log(i);
+    for (var j =6; j <9; j ++){
+        console.log('------', j)
+    } 
+}
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + ", " + secondNum);
+    }
+}
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + " times " + secondNum + " equals " + firstNum * secondNum);
+    }
+}
+for (var i = 100; i > 10; i = i - 10) {
+    for (var j = 10; j > 4; j = j - 5) {
+        console.log(i + " divided by " + j + " equals " + i / j);
+    }
+}
+var cubes = 'ABCDEFG';
+
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+//cubes.length syntax is the length of the string inside the var
+//cubes[i] snippet targets each letter in the loop based on value of i
+var cubes = 'ABCDEFG';
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+
